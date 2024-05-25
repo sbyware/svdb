5d5956ed9c8fb6fd4ebb5053310c6c1d0141e9e4
sbyware <91495085+sbyware@users.noreply.github.com>
2024-05-25 12:59:03 +0100

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # svdb
 
-the service database
+the service database (tcp & ucp).
 
 ## Installation
 
@@ -11,3 +11,64 @@ Copy `svdb` binary to a directory in your PATH. Place the `svdb.json` file in th
 ```bash
 svdb [-p <port,numbers,comma-separated>] [-m <regex-to-match>]
 ```
+
+Example:
+
+```bash
+svdb -p 80,443 -m "http"
+```
+
+## Configuration
+
+The `.svdb` file (copied to ~/.svdb) contains the list of services and their respective ports. The file is a JSON array with the following structure:
+
+```json
+[
+  {
+    "name": "http",
+    "port": 80
+  },
+  {
+    "name": "https",
+    "port": 443
+  }
+]
+```
+
+## the `-select` flag
+
+The `-select` flag allows you to filter the output of `svdb` by a specific field. The flag takes a string as an argument and will filter the output based on the value of the field.
+
+#### Get all services with port 80 or 443
+```bash
+svdb -p 80,443 -select port
+```
+
+#### Get all services with port 80 or 443
+```bash
+svdb -p 80,443 -select name
+```
+
+## the `-j` flag, with `jq`
+
+You can use `jq` to filter the output of `svdb` when using the `-j` flag.
+
+
+#### Get all services with port 80 or 443
+```bash
+svdb -j -p 80,443 | jq '.[] | select(.name == "http")'
+```
+
+#### Get all ports for services that match "http"
+```bash
+svdb -j -m "http" | jq -r '.[].port'
+```
+
+#### Get just the first description for services on port 443
+```bash
+svdb -j -p 443 | jq -r '.[].description' | head -n 1
+```
+
+## License
+
+MIT
--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"regexp"
 	"strings"
 )
 
-// Service represents a service entry in the database
+var (
+	DB_PATH    = fmt.Sprintf("%s/%s", os.Getenv("HOME"), ".svdb")
+	LOG_PREFIX = "[svdb]"
+)
+
 type Service struct {
 	Port        string `json:"port"`
 	Description string `json:"description"`
@@ -18,72 +23,212 @@ type Service struct {
 	Status      string `json:"status"`
 }
 
-var (
-	dbPath     = fmt.Sprintf("%s/%s", os.Getenv("HOME"), ".svdb")
-	logPrefix  = "[svdb]"
-	validFlags = []string{"p", "json", "m"}
-)
-
-// LoadDB loads the service database from JSON file
-func LoadDB(path string) (map[string][]Service, error) {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return nil, fmt.Errorf("%s database file does not exist", logPrefix)
+func LoadDB() (map[string][]Service, error) {
+	// if DB_PATH aint there, copy ./db.json to DB_PATH and return parsed db
+	if _, err := os.Stat(DB_PATH); os.IsNotExist(err) {
+		if err := CopyDB(); err != nil {
+			return nil, fmt.Errorf("%s error copying db: %s", LOG_PREFIX, err)
+		}
 	}
 
-	file, err := os.Open(path)
+	db, err := ParseDB()
 	if err != nil {
-		return nil, fmt.Errorf("%s error opening database file: %s", logPrefix, err)
-	}
-	defer file.Close()
-
-	var db map[string][]Service
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&db)
-	if err != nil {
-		return nil, fmt.Errorf("%s error decoding database file: %s", logPrefix, err)
+		return nil, fmt.Errorf("%s error parsing db: %s", LOG_PREFIX, err)
 	}
 
 	return db, nil
 }
 
+func CopyDB() error {
+	src, err := os.Open("./db.json")
+	if err != nil {
+		return fmt.Errorf("%s error opening db file: %s", LOG_PREFIX, err)
+	}
+	defer src.Close()
+
+	dst, err := os.Create(DB_PATH)
+	if err != nil {
+		return fmt.Errorf("%s error creating db file: %s", LOG_PREFIX, err)
+	}
+	defer dst.Close()
+
+	if _, err := io.Copy(dst, src); err != nil {
+		return fmt.Errorf("%s error copying db file: %s", LOG_PREFIX, err)
+	}
+
+	return nil
+}
+
+func ParseDB() (map[string][]Service, error) {
+	var db map[string][]Service
+
+	file, err := os.Open(DB_PATH)
+
+	if err != nil {
+		return nil, fmt.Errorf("%s error opening db file: %s", LOG_PREFIX, err)
+	}
+	decoder := json.NewDecoder(file)
+
+	if err := decoder.Decode(&db); err != nil {
+		return nil, fmt.Errorf("%s error decoding db file: %s", LOG_PREFIX, err)
+	}
+
+	defer file.Close()
+
+	return db, nil
+}
+
 func main() {
-	db, err := LoadDB(dbPath)
+	portFlag := flag.String("p", "", "query by (-p)ort number(s) (comma separated list of ports)")
+	jsonOutputFlag := flag.Bool("j", false, "output in (-j)son format")
+	matchFlag := flag.String("X", "", "query service database by regular e(-X)pression pattern matching")
+	filterFlag := flag.String("select", "", "reduce output to (-select=fields,like,so)")
+	flag.Parse()
+
+	db, err := LoadDB()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	port := flag.String("p", "", "query by port number(s) (comma separated list of ports)")
-	jsonOutput := flag.Bool("json", false, "output in json format")
-	match := flag.String("m", "", "query service database by regular expression pattern matching")
-	flag.Parse()
+	var matchedServices []Service
 
-	if !validateFlags(flag.CommandLine) {
+	switch {
+	case *portFlag != "":
+		services, err := queryByPort(db, *portFlag)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		matchedServices = services
+	case *matchFlag != "":
+		services, err := queryByMatch(db, *matchFlag)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		matchedServices = services
+	default:
+		fmt.Println("svdb, the service database.")
 		flag.PrintDefaults()
 		return
 	}
 
-	switch {
-	case *port != "":
-		queryByPort(db, *port, *jsonOutput)
-	case *match != "":
-		queryByMatch(db, *match, *jsonOutput)
-	default:
-		fmt.Println("svdb, the service database.")
-		flag.PrintDefaults()
+	if *filterFlag != "" {
+		matchedServices = selectFields(matchedServices, *filterFlag)
+		printFiltered(matchedServices)
+		return
+	}
+
+	if *jsonOutputFlag {
+		printJSON(matchedServices)
+	} else {
+		printPlain(matchedServices)
 	}
 }
 
-// validateFlags checks if the provided flags are valid
-func validateFlags(f *flag.FlagSet) bool {
-	valid := true
-	f.Visit(func(flag *flag.Flag) {
-		if !contains(validFlags, flag.Name) {
-			fmt.Printf("%s error: invalid flag %s\n", logPrefix, flag.Name)
-			valid = false
+// selectFields filters services based on specified fields
+func selectFields(services []Service, fields string) []Service {
+	if fields == "" {
+		return services
+	}
+
+	fieldsArr := strings.Split(fields, ",")
+
+	filteredServices := make([]Service, len(services))
+	for i, s := range services {
+		filteredServices[i] = Service{}
+		for _, f := range fieldsArr {
+			switch f {
+			case "port":
+				filteredServices[i].Port = s.Port
+			case "description":
+				filteredServices[i].Description = s.Description
+			case "tcp":
+				filteredServices[i].Tcp = s.Tcp
+			case "udp":
+				filteredServices[i].Udp = s.Udp
+			case "status":
+				filteredServices[i].Status = s.Status
+			}
 		}
-	})
-	return valid
+	}
+
+	return filteredServices
+}
+
+// queryByPort queries the database by port number
+func queryByPort(db map[string][]Service, port string) ([]Service, error) {
+	var services []Service
+	for _, p := range strings.Split(port, ",") {
+		foundServices, exists := db[p]
+		if !exists {
+			fmt.Printf("%s service with port '%s' not found in db\n", LOG_PREFIX, p)
+			continue
+		}
+		services = append(services, foundServices...)
+	}
+	return services, nil
+}
+
+// queryByMatch queries the database by regular expression pattern matching
+func queryByMatch(db map[string][]Service, pattern string) ([]Service, error) {
+	var services []Service
+
+	r, err := regexp.Compile(pattern)
+	if err != nil {
+		return nil, fmt.Errorf("%s error compiling regular expression: %s", LOG_PREFIX, err)
+	}
+
+	for _, v := range db {
+		for _, s := range v {
+			descProto := fmt.Sprintf("%s %s", strings.ToLower(s.Description), getProtocolString(s.Tcp, s.Udp))
+			if r.MatchString(descProto) {
+				services = append(services, s)
+			}
+		}
+	}
+
+	return services, nil
+}
+
+// printJSON prints services in JSON format
+func printJSON(services []Service) {
+	jsonData, err := json.MarshalIndent(services, "", "  ")
+	if err != nil {
+		fmt.Printf("%s error marshalling JSON: %s\n", LOG_PREFIX, err)
+		return
+	}
+	fmt.Println(string(jsonData))
+}
+
+// printPlain prints services in human-readable format
+func printPlain(services []Service) {
+	for _, s := range services {
+		proto := getProtocolString(s.Tcp, s.Udp)
+		printKeyVal("port", s.Port)
+		printKeyVal("description", s.Description)
+		printKeyVal("protocol", proto)
+		printKeyVal("status", s.Status)
+		fmt.Println()
+	}
+}
+
+func printFiltered(services []Service) {
+	// if theres more than 1 service and there's more than 1 key in each service,
+	// we call printPlain.
+	// else, means we only have 1 key in a single service, so we print just the value.
+	if len(services) > 1 && len(serviceToArr(services[0])) > 1 {
+		printPlain(services)
+	} else {
+		for _, s := range services {
+			fmt.Println(serviceToArr(s)[0])
+		}
+	}
+}
+
+func serviceToArr(s Service) []string {
+	return []string{s.Port, s.Description, getProtocolString(s.Tcp, s.Udp), s.Status}
 }
 
 // contains checks if a string slice contains a specific string
@@ -96,86 +241,24 @@ func contains(slice []string, str string) bool {
 	return false
 }
 
-// queryByPort queries the database by port number(s)
-func queryByPort(db map[string][]Service, port string, jsonOutput bool) {
-	ports := strings.Split(port, ",")
-	var services []Service
-
-	for _, p := range ports {
-		foundServices, exists := db[p]
-		if exists {
-			services = append(services, foundServices...)
-		} else {
-			fmt.Printf("service with port '%s' not found in db\n\n", p)
-		}
-	}
-
-	if jsonOutput {
-		printJSON(services)
-	} else {
-		printServices(services)
-	}
-}
-
-// printJSON prints services in JSON format
-func printJSON(services []Service) {
-	jsonData, err := json.MarshalIndent(services, "", "  ")
-	if err != nil {
-		fmt.Printf("%s error marshalling JSON: %s\n", logPrefix, err)
-		return
-	}
-	fmt.Println(string(jsonData))
-}
-
-// printServices prints services in human-readable format
-func printServices(services []Service) {
-	for _, s := range services {
-		proto := ""
-		if s.Tcp && s.Udp {
-			proto = "tcp/udp"
-		} else if s.Tcp {
-			proto = "tcp"
-		} else if s.Udp {
-			proto = "udp"
-		}
-		fmt.Printf("port\t\t: %s\ndescription\t: %s\nprotocol\t: %s\nstatus\t\t: %s\n\n", s.Port, s.Description, proto, s.Status)
-	}
-}
-
-// queryByMatch queries the database by regular expression pattern matching
-func queryByMatch(db map[string][]Service, pattern string, jsonOutput bool) {
-	var services []Service
-
-	r, err := regexp.Compile(pattern)
-	if err != nil {
-		fmt.Printf("%s error compiling regular expression: %s\n", logPrefix, err)
-		return
-	}
-
-	for _, v := range db {
-		for _, s := range v {
-			descProto := fmt.Sprintf("%s %s", strings.ToLower(s.Description), getProtocolString(s.Tcp, s.Udp))
-			if r.MatchString(descProto) {
-				services = append(services, s)
-			}
-		}
-	}
-
-	if jsonOutput {
-		printJSON(services)
-	} else {
-		printServices(services)
-	}
-}
-
-// getProtocolString returns the protocol string based on TCP and UDP flags
 func getProtocolString(tcp, udp bool) string {
-	if tcp && udp {
+	switch {
+	case tcp && udp:
 		return "tcp/udp"
-	} else if tcp {
+	case tcp:
 		return "tcp"
-	} else if udp {
+	case udp:
 		return "udp"
+	default:
+		return ""
 	}
-	return ""
+}
+
+func printKeyVal(key, val string) {
+	// needs to be even with the key. and the : needs to be in same col every time
+	if val == "" {
+		return
+	}
+
+	fmt.Printf("%-15s: %s\n", key, val)
 }
